93aafcefac7379b755734078ecb41d5c3c6c5365
ToniPerezCastejon <perezcastejon18037@iesmarenostrum.com>
2017-05-12 15:28:13 +0200

    Agregada clase PrimosLógica (vacía)

---
+++ NavajaSuiza/Aplicación 1 Números Primos/PrimosLógica.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavajaSuiza.Aplicación_1_Números_Primos
+{
+    class PrimosLógica
+    {
+    }
+}
