160f28be4ca0dbfc3cd6a4bc338807b438447f7a
ToniPerezCastejon <perezcastejon18037@iesmarenostrum.com>
2017-05-13 20:36:06 +0200

    Agregado Proyecto de Pruebas Unitarias "TestNavajaSuiza" a la Solución:
    - Agregada y actualizada TestClass UnitTestNumerosPrimos

---
+++ TestNavajaSuiza/UnitTestNavajaSuiza.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNavajaSuiza
+{
+    [TestClass]
+    public class UnitTestNavajaSuiza
+    {
+
+        [TestClass]
+        public class UnitTestNumerosPrimos
+        {
+
+            [TestMethod]
+            public void TestComprobarPrimo_Numero_1()
+            {
+                //escenario
+                int iNumero = 1;
+                bool sResultEsperado = true;
+                bool sResultObtenido;
+
+                //acción
+                sResultObtenido = NavajaSuiza.Aplicación_1_NumerosPrimos.PrimosLógica.ComprobarPrimo(iNumero);
+
+                //afirmación
+                Assert.AreEqual(sResultEsperado, sResultObtenido);
+            }
+
+            [TestMethod]
+            public void TestComprobarPrimo_Numero_2()
+            {
+                //escenario
+                int iNumero = 2;
+                bool sResultEsperado = true;
+                bool sResultObtenido;
+
+                //acción
+                sResultObtenido = NavajaSuiza.Aplicación_1_NumerosPrimos.PrimosLógica.ComprobarPrimo(iNumero);
+
+                //afirmación
+                Assert.AreEqual(sResultEsperado, sResultObtenido);
+            }
+
+            [TestMethod]
+            public void TestComprobarPrimo_Numero_3()
+            {
+                //escenario
+                int iNumero = 3;
+                bool sResultEsperado = true;
+                bool sResultObtenido;
+
+                //acción
+                sResultObtenido = NavajaSuiza.Aplicación_1_NumerosPrimos.PrimosLógica.ComprobarPrimo(iNumero);
+
+                //afirmación
+                Assert.AreEqual(sResultEsperado, sResultObtenido);
+            }
+
+            [TestMethod]
+            public void TestComprobar_Primo_Numero_4()
+            {
+                //escenario
+                int iNumero = 4;
+                bool sResultEsperado = false;
+                bool sResultObtenido;
+
+                //acción
+                sResultObtenido = NavajaSuiza.Aplicación_1_NumerosPrimos.PrimosLógica.ComprobarPrimo(iNumero);
+
+                //afirmación
+                Assert.AreEqual(sResultEsperado, sResultObtenido);
+            }
+
+            [TestMethod]
+            public void TestComprobarPrimo_Numero_MaxValueMenos1()
+            {
+                //escenario
+                int iNumero = Int32.MaxValue - 1;
+                bool sResultEsperado = false;
+                bool sResultObtenido;
+
+                //acción
+                sResultObtenido = NavajaSuiza.Aplicación_1_NumerosPrimos.PrimosLógica.ComprobarPrimo(iNumero);
+
+                //afirmación
+                Assert.AreEqual(sResultEsperado, sResultObtenido);
+            }
+
+            [TestMethod]
+            public void TestComprobarPrimo_Numero_MaxValue()
+            {
+                //escenario
+                int iNumero = Int32.MaxValue;
+                bool sResultEsperado = true;
+                bool sResultObtenido;
+
+                //acción
+                sResultObtenido = NavajaSuiza.Aplicación_1_NumerosPrimos.PrimosLógica.ComprobarPrimo(iNumero);
+
+                //afirmación
+                Assert.AreEqual(sResultEsperado, sResultObtenido);
+            }
+        }
+
+    }
+}
